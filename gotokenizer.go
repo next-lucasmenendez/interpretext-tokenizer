cf591eeeceb9f4144e3112afac5f7a2c8c72b6df
lucasmenendez <epucas@gmail.com>
2018-02-15 13:20:32 +0100

    Tokenizing puntuation symbols

--- gotokenizer.go
+++ gotokenizer.go
@@ -4,6 +4,7 @@ import "regexp"
 
 func Sentences(s string) (sentences []string) {
 	var (
+		// Patterns
 		numP       = regexp.MustCompile(`([0-9]+)\.([0-9]+)`)
 		quoutesP   = regexp.MustCompile(`("|'|“|”|’|«|»)`)
 		pstopsP    = regexp.MustCompile(`"(.+)\.(.+)"`)
@@ -12,8 +13,9 @@ func Sentences(s string) (sentences []string) {
 		resP       = regexp.MustCompile(`\*\|\*`)
 		dotP       = regexp.MustCompile(`{stop}`)
 
+		// Exchangers
 		noNum     = numP.ReplaceAllString(s, `$1*|*$2`)
-		noQuoutes = quoutesP.ReplaceAllString(noNum, "\"")
+		noQuoutes = quoutesP.ReplaceAllString(noNum, `"`)
 		noPstops  = pstopsP.ReplaceAllString(noQuoutes, "\"$1{partial_stop}$2\"")
 		noStops   = stopsP.ReplaceAllString(noPstops, `$0{stop}`)
 		text      = resP.ReplaceAllString(noStops, `.`)
@@ -24,17 +26,24 @@ func Sentences(s string) (sentences []string) {
 	return sentences
 }
 
-func Words(s string) (tokens []string) {
+func Words(rs string) (tokens []string) {
 	var (
-		ws = regexp.MustCompile(`\s|\t`)
-		cr = regexp.MustCompile(`\s|\t|"|'|\.|,|\:`)
-
-		temp = ws.Split(s, -1)
+		rgxS = regexp.MustCompile(`\s|\t`)
+		rgxD = regexp.MustCompile(`("|\.\.\.|\.|,|:|[0-9]+\.[0-9]+)`)
+		s = rgxS.Split(rs, -1)
 	)
 
-	for _, w := range temp {
-		var _w string = cr.ReplaceAllLiteralString(w, "")
-		if _w != "" {
+	for _, w := range s {
+		if rgxD.MatchString(w) {
+			processed := rgxD.ReplaceAllString(w, ` $1 `)
+			temps := rgxS.Split(processed, -1)
+
+			for _, t := range temps {
+				if len(t) > 0 {
+					tokens = append(tokens, t)
+				}
+			}
+		} else if len(w) > 0 {
 			tokens = append(tokens, w)
 		}
 	}
